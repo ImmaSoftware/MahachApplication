5e22842a8695a8212dc1e15dddc95f32dc03123d
ImmaSoftware <immasoftware@mail.ru>
2014-12-01 22:14:38 +0300

    Мой второй коммит

--- app/src/main/java/com/example/my/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/example/my/myapplication2/app/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.my.myapplication2.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "Hello Git", Toast.LENGTH_LONG);
+
     }
 
 }
